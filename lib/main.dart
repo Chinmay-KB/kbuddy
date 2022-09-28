8db415ab85c435efcbfa533f669d6f4f98a3a94e
Chinmay <chinmay.kabi@live.com>
2022-09-28 22:58:15 +0530

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
+import 'dart:developer';
 import 'dart:io';
+import 'dart:math' hide log;
 
 import 'package:audioplayers/audioplayers.dart';
+import 'package:ffmpeg_kit_flutter_audio/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_audio/return_code.dart';
+
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:record/record.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 final tuneUrl = 'https://www.youtube.com/watch?v=R2sxMVRgybI';
 final tuneManifest = 'R2sxMVRgybI';
+final record = Record();
+
 void main() {
   runApp(const MaterialApp(
     home: MyApp(),
@@ -27,22 +36,63 @@ class _MyAppState extends State<MyApp> {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: FutureBuilder<Map<String, dynamic>>(
-            future: fetchYoutubeData(),
-            builder: ((context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data.toString());
-              }
-              return const CircularProgressIndicator();
-            })),
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                playAudio(tuneManifest);
+              },
+              child: Text('Play Audio'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (!recordingAudio)
+                  recordAudio();
+                else
+                  stopRecording();
+              },
+              child: Text(
+                  recordingAudio ? 'Stop recording and play' : 'Record Audio'),
+            ),
+          ],
+        ),
       ),
     );
   }
 
   Future<Map<String, dynamic>> fetchYoutubeData() async {
     final video = await yt.videos.get(tuneUrl);
-    playAudio(tuneManifest);
     return {'name': video.title, 'author': video.author};
+  }
+
+  bool recordingAudio = false;
+  Future<void> recordAudio() async {
+    if (await Permission.microphone.request().isGranted) {
+      final directory = await getTemporaryDirectory();
+      final randomNo = Random().nextInt(1000).toString();
+      recordingPath = '${directory.path}/$randomNo.mp3';
+      setState(() {
+        recordingAudio = true;
+      });
+      await record.start(
+        path: recordingPath,
+        encoder: AudioEncoder.opus, // by default
+        bitRate: 128000, // by default
+        // by default
+      );
+    }
+  }
+
+  String? recordingPath;
+
+  Future<void> stopRecording() async {
+    final path = await record.stop();
+    setState(() {
+      recordingAudio = true;
+    });
+    if (path != null) {
+      mixBothAudios(path);
+    }
   }
 
   Future<void> playAudio(String manifest) async {
@@ -59,5 +109,36 @@ class _MyAppState extends State<MyApp> {
       final ap = AudioPlayer();
       ap.play(BytesSource(audioFile.readAsBytesSync()));
     });
+  }
+
+  Future<String> audioSource() async {
+    final manifest = await yt.videos.streamsClient.getManifest(tuneManifest);
+    final audioStreamInfo = manifest.audioOnly.withHighestBitrate();
+    var stream = yt.videos.streamsClient.get(audioStreamInfo);
+
+    final directory = await getTemporaryDirectory();
+    final audioFile = File('${directory.path}/audio.mp3');
+    var fileStream = audioFile.openWrite();
+    await stream.pipe(fileStream);
+    await fileStream.flush();
+    await fileStream.close();
+    return audioFile.path;
+  }
+
+  Future<void> mixBothAudios(String recording) async {
+    final directory = await getTemporaryDirectory();
+    final randomNo = Random().nextInt(1000).toString();
+    final outputPath = '${directory.path}/${randomNo}_output.mp3';
+    final youtubeAudioPath = await audioSource();
+    final result = await FFmpegKit.execute(
+        '-i ${youtubeAudioPath} -i ${recording} -filter_complex amix=inputs=2:duration=first $outputPath');
+    // ffmpeg
+    if (ReturnCode.isSuccess(await result.getReturnCode())) {
+      final ap = AudioPlayer();
+      ap.play(BytesSource(File(outputPath).readAsBytesSync()));
+    } else if (ReturnCode.isCancel(await result.getReturnCode())) {
+    } else {
+      log((await result.getAllLogsAsString()) ?? 'No logs');
+    }
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import ffmpeg_kit_flutter_audio
 import path_provider_macos
+import record_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RecordMacosPlugin.register(with: registry.registrar(forPlugin: "RecordMacosPlugin"))
 }
